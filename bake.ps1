13699189ce4a3affbe3335f2ad2de0765b9398ce
Serabass <serabass565@gmail.com>
2026-01-30 00:47:36 +0500

    1

--- bake.ps1
+++ bake.ps1
@@ -3,7 +3,6 @@ $startTime = Get-Date
 # --host "tcp://192.168.88.13:32375" `
 
 docker `
-  --host "tcp://192.168.88.13:32375" `
   buildx bake `
   --allow security.insecure `
   --file docker-bake.hcl `
